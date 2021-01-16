e1e4a9a449ec27552727ef7db2f8760b0271a7f9
Plofland <ploflan@gmail.com>
2021-01-15 22:18:06 -0500

    Adding styling to TaskOnboarding.js input and buttons

--- src/components/TaskOnboarding.js
+++ src/components/TaskOnboarding.js
@@ -3,18 +3,43 @@ import styled from 'styled-components';
 
 const StyledTaskOnboarding = styled.div`
   display: flex;
-  /* flex-direction: column; */
-  justify-content: space-around;
-  border: 2px solid black;
+  /* border: 2px solid black; */
 
   .addTask {
-    flex-direction: row;
-    border: 1px solid blue;
+    display: flex;
+    justify-content: center;
+    width: 60%;
+    margin: 1rem auto;
+    /* border: 1px solid blue; */
+
+    input {
+      width: 70%;
+    }
+
+    #addTaskBtn {
+      border-radius: 0 5px 5px 0;
+    }
   }
 
   .clearTasks {
-    flex-direction: row;
-    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 30%;
+    padding: 1%;
+    /* border: 1px solid red; */
+
+    button {
+      width: 80%;
+    }
+
+    #clearCompleteBtn {
+      border-radius: 8px 8px 0px 0;
+    }
+
+    #clearAllBtn {
+      border-radius: 0 0 8px 8px;
+    }
   }
 `;
 
@@ -25,6 +50,12 @@ export default function TaskOnboarding(props) {
 
   const handleChange = (e) => {
     setTask(e.target.value);
+
+    // setTask({
+    //   id: todoData.lenght,
+    //   task: e.target.value,
+    //   complete: false
+    // });
   };
 
   const handleSubmit = (e) => {
@@ -42,11 +73,13 @@ export default function TaskOnboarding(props) {
             value={task}
             onChange={handleChange}
           ></input>
-          <button onClick={handleSubmit}>Add Task</button>
+          <button id="addTaskBtn" onClick={handleSubmit}>
+            Add Task
+          </button>
         </div>
         <div className="clearTasks">
-          <button>Clear Completed Tasks</button>
-          <button>Clear All Tasks</button>
+          <button id="clearCompleteBtn">Clear Completed Tasks</button>
+          <button id="clearAllBtn">Clear All Tasks</button>
         </div>
       </StyledTaskOnboarding>
     </>
